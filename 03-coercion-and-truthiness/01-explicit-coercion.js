9d833277b97e1c074303b87e55fdc2ef013dc875
kyusulamita <sulamitamorales@yahoo.com>
2019-12-14 13:43:48 -0500

    new notes

---
+++ 03-coercion-and-truthiness/01-explicit-coercion.js
@@ -0,0 +1,8 @@
+/* eslint-disable quotes */
+// let string = "Cat"
+let string = "1000";
+console.log(string);
+
+let num = Number(string);
+
+console.log(num);
---
+++ 03-coercion-and-truthiness/04-conditionals.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-constant-condition */
+// console.log(Boolean("false"))
+// console.log(null == undefined)
+// console.log(NaN == null)
+// console.log(NaN == undefined)
+// console.log(NaN === NaN)
+
+// if("apples"){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+// if(Boolean("apples")){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+
+// if(""){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+// if(Boolean("")){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+// if(10){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+// if(Boolean(10)){
+//   console.log("in the if")
+// } else {
+//   console.log("else")
+// }
+
+
+// if (30 + 20 - 50){
+//   console.log('in the if')
+// } else {
+//   console.log('else')
+// }
+
+// if ((30 + 20 - 50)){
+//   console.log('in the if')
+// } else {
+//   console.log('else')
+// }
+
+// if ((30 + 20 - 50) === 30) {
+//   console.log('in the if')
+// } else {
+//   console.log('else')
+// }
+
+// if ('apples' === 'apples') {
+//   console.log('in the if')
+// } else {
+//   console.log('else')
+// }
